e868015fd2e180bf0124392c57056af624634422
r-amiri <65209767+r-amiri@users.noreply.github.com>
2020-11-18 13:51:06 +0900

    governance msg.rs file

---
+++ contracts/governance/src/msg.rs
@@ -0,0 +1,25 @@
+use cosmwasm_std::HumanAddr;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct InitMsg {
+    pub nameservice_code_id: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleMsg {
+    Subscribe { name: String },
+    Unsubscribe { name: String },
+    Signup {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    PaidAmountIs { address: HumanAddr },
+    AddressExists { address: HumanAddr },
+    GetNameServiceAddress {},
+}
