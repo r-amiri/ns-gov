b0dafed66dac7075f316c0745f6f023116ea64ed
r-amiri <65209767+r-amiri@users.noreply.github.com>
2020-11-18 14:01:33 +0900

    Optimized using docker in the artifacts directory

--- target/debug/deps/governance.d
+++ target/debug/deps/governance.d
@@ -1,8 +1,8 @@
-/Users/rita/my-second-contract/target/debug/deps/libgovernance.dylib: contracts/governance/src/lib.rs contracts/governance/src/contract.rs contracts/governance/src/msg.rs contracts/governance/src/state.rs
+/Users/rita/workspace/ns-gov/target/debug/deps/libgovernance.dylib: contracts/governance/src/lib.rs contracts/governance/src/contract.rs contracts/governance/src/msg.rs contracts/governance/src/state.rs
 
-/Users/rita/my-second-contract/target/debug/deps/libgovernance.rlib: contracts/governance/src/lib.rs contracts/governance/src/contract.rs contracts/governance/src/msg.rs contracts/governance/src/state.rs
+/Users/rita/workspace/ns-gov/target/debug/deps/libgovernance.rlib: contracts/governance/src/lib.rs contracts/governance/src/contract.rs contracts/governance/src/msg.rs contracts/governance/src/state.rs
 
-/Users/rita/my-second-contract/target/debug/deps/governance.d: contracts/governance/src/lib.rs contracts/governance/src/contract.rs contracts/governance/src/msg.rs contracts/governance/src/state.rs
+/Users/rita/workspace/ns-gov/target/debug/deps/governance.d: contracts/governance/src/lib.rs contracts/governance/src/contract.rs contracts/governance/src/msg.rs contracts/governance/src/state.rs
 
 contracts/governance/src/lib.rs:
 contracts/governance/src/contract.rs:
